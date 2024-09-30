59ef1cc7a52a334c0e01d5ba96655631ac7592ce
Treasurek04 <treasurekeys2004@gmail.com>
2024-09-29 23:06:27 -0500

    Update MoveLeft.cs

--- Prototype 3/Assets/Scripts/MoveLeft.cs
+++ Prototype 3/Assets/Scripts/MoveLeft.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 30f;
+    private float leftBound = -15f;
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+
+        //Destroy obstacles out of bound off screen to the left
+        if(transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject); 
+        }
     }
 }
