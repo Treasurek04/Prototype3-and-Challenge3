d73b5d87dea704208eb7e4beb48b967705eea87c
Treasurek04 <treasurekeys2004@gmail.com>
2024-10-01 14:26:30 -0500

    Update PlayerControllerX.cs

--- Challenge3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ Challenge3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -5,66 +5,78 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public bool gameOver = false;
-    public float floatForce = 5f;
-    private float gravityModifier = 1.2f;
-    private Rigidbody playerRb;
+    public float floatForce = 10f;
+    public float bounceForce = 5f;
+    public float gravityModifier = 1.2f;
+    public float maxY = 14f;
+    public float minY = 0.5f;
+
     public ParticleSystem explosionParticle;
     public ParticleSystem fireworksParticle;
     private AudioSource playerAudio;
     public AudioClip moneySound;
     public AudioClip explodeSound;
+    public AudioClip groundedSound;
+
+    private Rigidbody playerRb;
 
     void Start()
     {
-        // Get Rigidbody and AudioSource components
         playerRb = GetComponent<Rigidbody>();
         playerAudio = GetComponent<AudioSource>();
-
-        // Set gravity
         Physics.gravity *= gravityModifier;
-
-        // Give initial upward force to the balloon
-        playerRb.AddForce(Vector3.up * floatForce, ForceMode.Impulse);
+        playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
     }
 
     void Update()
     {
-        // Check for spacebar input to make the balloon float
-        if (Input.GetKey(KeyCode.Space) && !gameOver)
+        if (!gameOver)
         {
-            playerRb.AddForce(Vector3.up * floatForce * Time.deltaTime, ForceMode.Impulse);
-        }
-        else
-        {
-            playerRb.AddForce(Vector3.down * gravityModifier * Time.deltaTime);
+            if (Input.GetKey(KeyCode.Space) && transform.position.y < maxY)
+            {
+                playerRb.AddForce(Vector3.up * floatForce);
+            }
+
+            if (!Input.GetKey(KeyCode.Space))
+            {
+                playerRb.AddForce(Vector3.down * floatForce * Time.deltaTime, ForceMode.Acceleration);
+            }
+
+            if (transform.position.y > maxY)
+            {
+                playerRb.position = new Vector3(playerRb.position.x, maxY, playerRb.position.z);
+                playerRb.velocity = new Vector3(playerRb.velocity.x, 0, playerRb.velocity.z);
+            }
+
+            if (transform.position.y < minY)
+            {
+                playerRb.position = new Vector3(playerRb.position.x, minY, playerRb.position.z);
+                playerRb.AddForce(Vector3.up * bounceForce, ForceMode.Impulse);
+                playerAudio.PlayOneShot(groundedSound, 1.0f);
+            }
         }
 
-        // Prevent the balloon from floating off screen
-        if (transform.position.y > 14 && playerRb.velocity.y > 0)
+        if (gameOver && Input.GetKeyDown(KeyCode.R))
         {
-            playerRb.velocity = new Vector3(playerRb.velocity.x, 0, playerRb.velocity.z);
+            UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
         }
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        // Handle collision with Bomb
         if (other.gameObject.CompareTag("Bomb"))
         {
             explosionParticle.Play();
             playerAudio.PlayOneShot(explodeSound, 1.0f);
             gameOver = true;
-            Debug.Log("Game Over!");
             Destroy(other.gameObject);
         }
 
-        // Handle collision with Money
-        if (other.gameObject.CompareTag("Money"))
+        else if (other.gameObject.CompareTag("Money"))
         {
             fireworksParticle.Play();
             playerAudio.PlayOneShot(moneySound, 1.0f);
             Destroy(other.gameObject);
         }
-
     }
 }
