a0cf6dd409f1820486473639e33147aeace0de94
Treasurek04 <treasurekeys2004@gmail.com>
2024-09-30 21:44:08 -0500

    Working on issues

--- Challenge3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ Challenge3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -5,37 +5,26 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public bool gameOver;
-
     public float floatForce;
     private float gravityModifier = 1.2f;
     private Rigidbody playerRb;
-
     public ParticleSystem explosionParticle;
     public ParticleSystem fireworksParticle;
-
     private AudioSource playerAudio;
     public AudioClip moneySound;
     public AudioClip explodeSound;
 
-
-    // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-
         Physics.gravity *= gravityModifier;
         playerAudio = GetComponent<AudioSource>();
-
-        // Apply a small upward force at the start of the game
-        playerRb.AddForce(Vector3.up * 5);
-
+        playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // While space is pressed and player is low enough, float up
-        if (Input.GetKey(KeyCode.Space) && !gameOver && transform.position.y < 15)
+        if (Input.GetKey(KeyCode.Space) && !gameOver)
         {
             playerRb.AddForce(Vector3.up * floatForce * Time.deltaTime, ForceMode.Impulse);
         }
@@ -43,7 +32,6 @@ public class PlayerControllerX : MonoBehaviour
 
     private void OnCollisionEnter(Collision other)
     {
-        // if player collides with bomb, explode and set gameOver to true
         if (other.gameObject.CompareTag("Bomb"))
         {
             explosionParticle.Play();
@@ -51,17 +39,12 @@ public class PlayerControllerX : MonoBehaviour
             gameOver = true;
             Debug.Log("Game Over!");
             Destroy(other.gameObject);
-        } 
-
-        // if player collides with money, fireworks
+        }
         else if (other.gameObject.CompareTag("Money"))
         {
             fireworksParticle.Play();
             playerAudio.PlayOneShot(moneySound, 1.0f);
             Destroy(other.gameObject);
-
         }
-
     }
-
 }
---
+++ Challenge3/Assets/Challenge 3/Scripts/UIManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public int score = 0;
+    public Text scoreText;
+    public PlayerControllerX playerControllerXScript;
+    public bool won = false;
+
+    void Start()
+    {
+        if (scoreText == null)
+        {
+            scoreText = FindObjectOfType<Text>();
+        }
+
+        if (playerControllerXScript == null)
+        {
+            playerControllerXScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControllerX>();
+        }
+
+        scoreText.text = "Score: " + score;
+    }
+
+    void Update()
+    {
+        if (!playerControllerXScript.gameOver)
+        {
+            scoreText.text = "Score: " + score;
+
+            // Check for win condition
+            if (score >= 10 && !won)
+            {
+                playerControllerXScript.gameOver = true;
+                won = true;
+                scoreText.text = "You Win!\nPress R to Try Again!";
+            }
+        }
+
+        if (playerControllerXScript.gameOver && !won)
+        {
+            scoreText.text = "You Lose!\nPress R to Try Again!";
+        }
+
+        if (playerControllerXScript.gameOver && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    // Method to add score
+    public void AddScore()
+    {
+        score++;
+        Debug.Log("Score: " + score);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Money"))
+        {
+            AddScore(); // Increment score when collecting money
+            Destroy(other.gameObject); // Destroy the money object after collecting it
+        }
+    }
+}
